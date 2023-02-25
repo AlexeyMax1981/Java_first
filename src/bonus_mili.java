f7195c5fd8949fdda7b3b8664e5785964bf8fb63
Alexey 1981 <maximenko.av@rambler.ru>
2023-02-25 20:42:10 +0300

    add bonus-mili

---
+++ src/bonus_mili.java
@@ -0,0 +1,10 @@
+public class bonus_mili {
+    public static void main(String[] args) {
+        int x = 120;
+        int s = x / 20;
+            System.out.println("Количество бонусных миль = " + s);
+        }
+
+
+    }
+
