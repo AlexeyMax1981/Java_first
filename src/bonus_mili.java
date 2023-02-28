18b4fa5210ca5a37fa40a589c9b8d474103c5743
Alexey 1981 <maximenko.av@rambler.ru>
2023-02-28 05:33:12 +0300

    add auto_code

--- src/bonus_mili.java
+++ src/bonus_mili.java
@@ -2,9 +2,9 @@ public class bonus_mili {
     public static void main(String[] args) {
         int myTicketPrice = 120;
         int myBonus = myTicketPrice / 20;
-            System.out.println("Количество бонусных миль = " + myBonus);
-        }
-
-
+        System.out.println("Количество бонусных миль = " + myBonus);
     }
+
+
+}
 
